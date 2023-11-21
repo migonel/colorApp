b874e1fd31af6e087910924e00ecc27359d1d574
Lucas <migonel55@gmail.com>
2023-11-21 17:07:35 -0300

    color app first commit

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import Crashes from 'appcenter-crashes';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <View>
+        <Button title="Crash" onPress={() => Crashes.generateTestCrash("test")}/>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
